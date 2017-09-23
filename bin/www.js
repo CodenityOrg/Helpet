5e89eae4b275cb071594a9bbaeaa76c87ff74553
Angel Rodriguez Romero <aleangrodriguez123@gmail.com>
2017-09-23 09:50:05 -0500

    First Deploy

--- bin/www.js
+++ bin/www.js
@@ -1,6 +1,6 @@
 const server = require("../app.js").serv;
-const port = process.env.port || 3000;
-const hostname = process.env.host || "127.0.0.1";
+const port = process.env.PORT || 3000;
+const hostname = process.env.HOST || "127.0.0.1";
 
 server.listen(port,hostname, () => {
 	console.log(`Server running on http://${hostname}:${port}`);
