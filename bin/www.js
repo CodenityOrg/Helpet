18c285925ddfa69af95cb92f0e52401e458bf767
Angel Rodriguez Romero <aleangrodriguez123@gmail.com>
2017-09-29 21:06:06 -0500

    change config

--- bin/www.js
+++ bin/www.js
@@ -1,6 +1,6 @@
 const server = require("../app.js").serv;
 const port = process.env.PORT || 3000;
-const hostname = process.env.HOST || "localhost";
+const hostname = process.env.HOST || "0.0.0.0";
 
 server.listen(port, hostname, () => {
 	console.log(`Server running on http://${hostname}:${port}`);
